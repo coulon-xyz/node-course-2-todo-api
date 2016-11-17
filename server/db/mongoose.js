557dc28f4740b0008fcb3965ee1559f2e02b6f85
Coulon <julien.j.coulon@gmail.com>
2016-11-17 11:19:40 +0100

    Setup app for heroku 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise; // telling mongoose to use build in promises and not a third party framework
 
-mongoose.connect(prcess.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // will wait for connection before continuing
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); 
 
 module.exports = {mongoose};
