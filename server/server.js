a11c31425c3981fc2394e3422a358a032617a1e6
Coulon <julien.j.coulon@gmail.com>
2016-11-17 11:21:40 +0100

    Setup app for heroku 3

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const {ObjectID} = require('mongodb')
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/User');
+var {User} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000
