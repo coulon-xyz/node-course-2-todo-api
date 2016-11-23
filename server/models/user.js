245cb06298c2d7c0a109ea114f42ebb3d100a454
Coulon <julien.j.coulon@gmail.com>
2016-11-23 15:14:20 +0100

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -77,6 +77,29 @@ return User.findOne(searchObject);
 
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user){
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+
+
+      console.log(user.password)
+        // Use bcrypt.compare to compare password and user.password
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject();
+          }
+        })
+    })
+
+  });
+};
+
 // mongoose middleware
 UserSchema.pre('save', function (next) {
   var user = this;
--- server/server.js
+++ server/server.js
@@ -116,6 +116,23 @@ app.post('/users',(req,res) => {
   })
 });
 
+// POST /users/login {email, password}
+
+app.post('/users/login', (req,res) => {
+
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+
+});
+
 // Private routes
 
 app.get('/users/me', authenticate, (req, res) => {
