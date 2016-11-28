964f500d847d4aecb38bf415170cc67a0487fef5
Coulon <julien.j.coulon@gmail.com>
2016-11-28 15:10:05 +0100

    Add tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -295,27 +295,50 @@ describe ('POST /users/login', () => {
       });
   });
 
-it('should reject invalid login', (done) => {
-  request(app)
-    .post('/users/login')
-    .send({
-      email: users[1].email,
-      password: "wrongPassword"
-    })
-    .expect(400)
-    .expect((res) => {
-      expect(res.headers['x-auth']).toNotExist();
-    })
-    .end((err,res) => {
-      if (err) {
-        return done(err);
-      }
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: "wrongPassword"
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err,res) => {
+        if (err) {
+          return done(err);
+        }
 
-      User.findById(users[1]._id).then((user) => {
+        User.findById(users[1]._id).then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+        }).catch((e) => done(e));
+      });
+  });
+});
+
+describe ('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    // DELETE /users/me/token
+    // Set x-auth equalt to token
+    // 200
+    // Find user, verify that token has length of zero
+
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res)=> {
+        if (err) {
+          return done(err)
+        }
+        User.findById(users[0]._id).then((user) => {
           expect(user.tokens.length).toBe(0);
           done();
-      }).catch((e) => done(e));
+        }).catch((e) => done(e));
+      })
     });
-});
 
 });
